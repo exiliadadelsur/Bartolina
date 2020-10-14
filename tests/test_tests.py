12d93cf9f6d482258e33d1bc094436e0cb9b2b2a
Claudius <yoclaudioantonio@yahoo.com.ar>
2020-10-14 11:01:10 -0300

    numHalo test

---
+++ tests/test_tests.py
@@ -0,0 +1,15 @@
+import numpy as np
+import bartolina as bt
+from astropy.table import Table
+
+
+def test_numHalo():
+
+    gal = Table.read("resources/SDSS.fits")
+    obj = bt.ReZSpace(gal["RAJ2000"], gal["DEJ2000"], gal["z"],
+                      100.0, 0.27, 0.73)
+    unique_elements, counts_elements = np.unique(
+        obj.clustering.labels_, return_counts=True
+    )
+    canthalo = np.sum([counts_elements > 150])
+    assert canthalo == 15
