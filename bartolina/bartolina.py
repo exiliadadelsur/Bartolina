caa05349f3fd0127a57a2e37a8cb21cea7b0a6c8
exiliadadelsur <noeliarocioperez@hotmail.com>
2020-10-15 11:17:48 -0300

    we added attrs

--- bartolina/bartolina.py
+++ bartolina/bartolina.py
@@ -3,68 +3,77 @@ from astropy.cosmology import LambdaCDM
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-# from astropy import constants as const
-# from cluster_toolkit import bias
 from sklearn.cluster import DBSCAN
 
+# from cluster_toolkit import bias
+# import camb
+# from camb import model
 
-class ReZSpace:
-    def __init__(self, ra, dec, z, Ho, omega_m, omega_lambda):
-        self.ra = ra
-        self.dec = dec
-        self.z = z
-        self.Ho = Ho
-        self.omega_m = omega_m
-        self.omega_lambda = omega_lambda
-        self.Mth = 10 ** 12.5
+import attr
 
-        # cosmology is established
-        cosmo = LambdaCDM(H0=self.Ho, Om0=self.omega_m, Ode0=self.omega_lambda)
+
+@attr.s
+class ReZSpace(object):
+
+    ra = attr.ib()
+    dec = attr.ib()
+    z = attr.ib()
+    H0 = attr.ib()
+    omega_m = attr.ib()
+    omega_lambda = attr.ib()
+    Mth = attr.ib(default=(10 ** 12.5))
+
+    def mclustering(self):
+        cosmo = LambdaCDM(H0=self.H0, Om0=self.omega_m, Ode0=self.omega_lambda)
         dc = cosmo.comoving_distance(self.z)
         c = SkyCoord(
             ra=np.array(self.ra) * u.degree,
             dec=np.array(self.dec) * u.degree,
             distance=np.array(dc) * u.mpc,
         )
-        # coordinates transform
         xyz = np.array([c.cartesian.x, c.cartesian.y, c.cartesian.z]).T
         pesos = 1 + np.arctan(self.z / 0.050)
         self.clustering = DBSCAN(eps=3, min_samples=130)
         self.clustering.fit(xyz, sample_weight=pesos)
 
     # reconstructed Kaiser space; based on correcting for FOG effect only
-    # def ReKaiserSpace(self):
+#    def ReKaiserSpace(self):
 
-    # Grillado 3D
+#       Grillado 3D
 
-    # Halo bias
-    # calcular k y P_linear con CAMB
-    # bhm = bias.bias_at_M(self.Mth, k, P_linear, self.omega_m)
+#       Halo bias
+#       calcular k y P_linear con CAMB
+#       cosmología
+#       pars = camb.CAMBparams()
+#       pars.set_cosmology(self.H0, ombh2=0.022, omch2=0.122)
+#       pars.set_dark_energy(w=-1.0)
+#       pars.InitPower.set_params(ns=0.965)
+#       pars.set_matter_power(redshifts=[0.0, 0.8], kmax=2.0)
+#       calcula plineal
+#       pars.NonLinear = model.NonLinear_none
+#       results = camb.get_results(pars)
+#       kh, z, pk = results.get_matter_power_spectrum(minkh=1e-4, maxkh=1,
+#                                                     npoints=1000)
+#       bhm = bias.bias_at_M(self.Mth, kh, pk, self.omega_m)
+#       Mass density contrast
+#       deltah =
+#       Calculo de f
+#       f = self.omega_m ** 0.6 + 1 / 70 * self.omega_lambda *
+#       (1 + self.omega_m)
+#       ReKaiserZ
+#       zk = (self.z - v/const.c) / 1 + v/const.c
+#       Comoving distance
+#       rcomovingk = calculo de distancia comoving a partir de zk
+#       return #rcomovingk
 
-    # Mass density contrast
-    # deltah =
+#   reconstructed FOG space; based on correcting for Kaiser effect only
+#   def ReFoGSpace(self):
+#       return rcomovingf
 
-    # Calculo de f
-    # f = (self.omega_m ** 0.6 + 1 / 70 * self.omega_lambda *
-    #     (1 + self.omega_m))
-
-    # ReKaiserZ
-    # zk = (self.z - v/const.c) / 1 + v/const.c
-
-    # Comoving distance
-    # rcomovingk = calculo de distancia comoving a partir de zk
-
-    # return #rcomovingk
-    # reconstructed FOG space; based on correcting for Kaiser effect only
-    # def ReFoGSpace(self):
-
-    # return rcomovingf
-    # Re-real space reconstructed real space; based on correcting redshift
-    # space distortions
-    # def ReRealSpace(self):
-
-    # llama a ReKaiserSpace + ReFoGSpace
-    # rcomovingk = ReKaiserSpace()
-    # rcomovingf = ReFoGSpace()
-
-    # return rcomovingk + rcomovingf
+#    Re-real space reconstructed real space; based on correcting redshift
+#    space distortions
+#    def ReRealSpace(self):
+#       llama a ReKaiserSpace + ReFoGSpace
+#       rcomovingk = ReKaiserSpace()
+#       rcomovingf = ReFoGSpace()
+#       return rcomovingk + rcomovingf
