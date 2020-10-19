4b8a9ba541cdbf52e1d6b797032bcaa2f9f7d3d3
Claudius <yoclaudioantonio@yahoo.com.ar>
2020-10-18 22:48:24 -0300

    halo mass test

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -6,19 +6,18 @@ from astropy.table import Table
 def test_numHalo():
 
     gal = Table.read("resources/SDSS.fits")
-    obj = bt.ReZSpace(gal["RAJ2000"], gal["DEJ2000"], gal["z"],
-                      100.0, 0.27, 0.73)
+    obj = bt.ReZSpace(gal["RAJ2000"], gal["DEJ2000"], gal["z"])
     obj.mclustering()
     unique_elements, counts_elements = np.unique(
         obj.clustering.labels_, return_counts=True
     )
     canthalo = np.sum([counts_elements > 150])
-    assert canthalo == 14
+    assert canthalo == 15
+
 
 def test_hmass():
-    
+
     gal = Table.read("resources/SDSS.fits")
-    obj = bt.ReZSpace(gal["RAJ2000"], gal["DEJ2000"], gal["z"],
-                      100.0, 0.27, 0.73)
+    obj = bt.ReZSpace(gal["RAJ2000"], gal["DEJ2000"], gal["z"])
+    obj.mclustering()
     assert len(obj.hmass) == 24
-   
